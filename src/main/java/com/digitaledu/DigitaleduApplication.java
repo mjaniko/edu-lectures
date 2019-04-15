aead04d730e722b0693f7392afcde8eda085876d
Mikheil Janiashvili <mjaniko@gmail.com>
2019-04-15 20:32:07 +0400

    Initial Starting Project

---
+++ src/main/java/com/digitaledu/DigitaleduApplication.java
@@ -0,0 +1,13 @@
+package com.digitaledu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitaleduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitaleduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitaledu/DigitaleduApplicationTests.java
@@ -0,0 +1,16 @@
+package com.digitaledu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DigitaleduApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
